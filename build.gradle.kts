6de919d0cf68cfa0e8bf6145d5a451f08eae7399
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-11-29 14:55:33 +0100

    Bump dependencies to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,17 @@ version = "1.0.0"
 
 object Versions {
     const val coroutines = "1.5.2"
+    const val flyway = "8.1.0"
+    const val hikari = "5.0.0"
     const val jackson = "2.13.0"
-    const val flyway = "7.15.0"
-    const val hikari = "3.4.5"
     const val kafka = "2.7.0"
     const val kafkaEmbeddedEnv = "2.5.0"
     const val kluent = "1.68"
     const val ktor = "1.6.5"
-    const val logback = "1.2.6"
-    const val logstashEncoder = "6.6"
-    const val mockk = "1.12.0"
-    const val nimbusjosejwt = "7.5.1"
+    const val logback = "1.2.7"
+    const val logstashEncoder = "7.0.1"
+    const val mockk = "1.12.1"
+    const val nimbusjosejwt = "9.15.2"
     const val postgres = "42.3.1"
     const val postgresEmbedded = "0.13.4"
     const val prometheus = "0.9.0"
@@ -25,9 +25,9 @@ object Versions {
 }
 
 plugins {
-    kotlin("jvm") version "1.5.10"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
+    kotlin("jvm") version "1.6.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
 repositories {
