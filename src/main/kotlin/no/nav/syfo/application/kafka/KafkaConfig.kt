3f1dd913e8f1724012aabb849dd6d9df0630e960
Eirik Dahlen <37441744+eirikdahlen@users.noreply.github.com>
2023-01-31 08:33:30 +0100

    Add missing key deserializer (#86)

--- src/main/kotlin/no/nav/syfo/application/kafka/KafkaConfig.kt
+++ src/main/kotlin/no/nav/syfo/application/kafka/KafkaConfig.kt
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.config.SaslConfigs
 import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.serialization.StringDeserializer
 import java.util.*
 
 fun commonKafkaAivenConfig(
@@ -27,6 +28,7 @@ fun commonKafkaAivenConsumerConfig(
 ) = Properties().apply {
     putAll(commonKafkaAivenConfig(kafkaEnvironment))
     this[ConsumerConfig.GROUP_ID_CONFIG] = "ispengestopp-v1"
+    this[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.canonicalName
     this[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
     this[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = "false"
     this[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "1000"
