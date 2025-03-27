d0d3a189dc54e0651193d4f06aa22fc3ee62e331
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2025-03-27 15:01:53 +0100

    IS-3181: Fikset formatering

--- src/main/kotlin/no/nav/syfo/application/PengestoppService.kt
+++ src/main/kotlin/no/nav/syfo/application/PengestoppService.kt
@@ -26,9 +26,7 @@ class PengestoppService(
 
     private fun removeDeprecatedArsak(statusEndring: StatusEndring): StatusEndring {
         return statusEndring.copy(
-            arsakList = statusEndring.arsakList.filter {
-                !it.type.isDeprecated
-            }
+            arsakList = statusEndring.arsakList.filter { !it.type.isDeprecated }
         )
     }
 }
