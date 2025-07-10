d090fb60d1748096967050f7030f65a7a5451b93
geir-waagboe <geir.arne.waagbo@nav.no>
2025-07-10 11:59:31 +0200

    IS-3258: Use correct postgres version in tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ val mockk = "1.13.16"
 val nimbusjosejwt = "10.0.2"
 val postgres = "42.7.5"
 val postgresEmbedded = "2.1.0"
+val postgresRuntimeVersion = "17.5.0"
 val spek = "2.0.19"
 
 plugins {
@@ -64,6 +65,7 @@ dependencies {
     implementation("org.postgresql:postgresql:$postgres")
     implementation("com.zaxxer:HikariCP:$hikari")
     testImplementation("io.zonky.test:embedded-postgres:$postgresEmbedded")
+    testImplementation(platform("io.zonky.test.postgres:embedded-postgres-binaries-bom:$postgresRuntimeVersion"))
 
     // Kafka
     val excludeLog4j = fun ExternalModuleDependency.() {
